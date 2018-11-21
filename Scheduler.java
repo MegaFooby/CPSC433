4dc854caabd61f9ada5668b38545e51f3b5cef1f
MegaFooby <32686255+MegaFooby@users.noreply.github.com>
2018-11-21 14:18:01 -0700

    Fixed errors and changed collisions to hashmap
    Commented out collision arrays in case you want to use them instead

--- Scheduler.java
+++ Scheduler.java
@@ -3,7 +3,11 @@ import java.util.*;
 public class Scheduler {
 	//time is setup with the first digit as the day and the rest is the start time
 	//lecture with lecture collisions
-	public static final int lectime_collisions[][] = {
+	public static HashMap<Integer, Integer> lectime_collisions = new HashMap(21);
+	public static HashMap<Integer, Integer> tuttime_collisions = new HashMap(31);
+	public static HashMap<Integer, Integer> lectuttime_collisions = new HashMap(41);
+	
+	/*public static final int lectime_collisions[][] = {
 		//monday
 		{10800, 10800}, {10900, 10900}, {11000, 11000}, {11100, 11100},
 		{11200, 11200}, {11300, 11300}, {11400, 11400}, {11500, 11500},
@@ -12,7 +16,7 @@ public class Scheduler {
 		//tuesday
 		{20800, 20800}, {20930, 20930}, {21100, 21100}, {21230, 21230},
 		{21400, 21400}, {21530, 21530}, {21700, 21700}, {21830, 21830}
-	}
+	};
 	//tutorial with tutorial collisions
 	public static final int tuttime_collisions[][] = {
 		//monday
@@ -28,7 +32,7 @@ public class Scheduler {
 		//friday
 		{50800, 50800}, {51000, 51000}, {51200, 51200}, {51400, 51400},
 		{51600, 51600}
-	}
+	};
 	//lecture with tutorial collisions. lecutre times are first
 	public static final int lectuttime_collisions[][] = {
 		//monday
@@ -45,7 +49,7 @@ public class Scheduler {
 		{21100, 21100}, {21100, 21200}, {21230, 21200}, {21230, 21300},
 		{21400, 21400}, {21400, 21500}, {21530, 21500}, {21530, 21600},
 		{21700, 21700}, {21700, 21800}, {21830, 21800}, {21830, 21900}
-	}
+	};*/
 	public static final int times[] = {
 		//monday
 		10800, 10900, 11000, 11100, 11200, 11300, 11400, 11500, 11600,
@@ -57,9 +61,39 @@ public class Scheduler {
 		21700, 21800, 21900, 22000,
 		//friday
 		10800, 11000, 11200, 11400, 11600, 11800
-	}
+	};
 	
 	public static void main(String args[]) {
+		lectime_collisions.put(10800, 10800);lectime_collisions.put(10900, 10900);lectime_collisions.put(11000, 11000);lectime_collisions.put(11100, 11100);
+		lectime_collisions.put(11200, 11200);lectime_collisions.put(11300, 11300);lectime_collisions.put(11400, 11400);lectime_collisions.put(11500, 11500);
+		lectime_collisions.put(11600, 11600);lectime_collisions.put(11700, 11700);lectime_collisions.put(11800, 11800);lectime_collisions.put(11900, 11900);
+		lectime_collisions.put(12000, 12000);
+		lectime_collisions.put(20800, 20800);lectime_collisions.put(20930, 20930);lectime_collisions.put(21100, 21100);lectime_collisions.put(21230, 21230);
+		lectime_collisions.put(21400, 21400);lectime_collisions.put(21530, 21530);lectime_collisions.put(21700, 21700);lectime_collisions.put(21830, 21830);
+		
+		tuttime_collisions.put(10800, 10800);tuttime_collisions.put(10900, 10900);tuttime_collisions.put(11000, 11000);tuttime_collisions.put(11100, 11100);
+		tuttime_collisions.put(11200, 11200);tuttime_collisions.put(11300, 11300);tuttime_collisions.put(11400, 11400);tuttime_collisions.put(11500, 11500);
+		tuttime_collisions.put(11600, 11600);tuttime_collisions.put(11700, 11700);tuttime_collisions.put(11800, 11800);tuttime_collisions.put(11900, 11900);
+		tuttime_collisions.put(12000, 12000);
+		tuttime_collisions.put(20800, 20800);tuttime_collisions.put(20900, 20900);tuttime_collisions.put(21000, 21000);tuttime_collisions.put(21100, 21100);
+		tuttime_collisions.put(21200, 21200);tuttime_collisions.put(21300, 21300);tuttime_collisions.put(21400, 21400);tuttime_collisions.put(21500, 21500);
+		tuttime_collisions.put(21600, 21600);tuttime_collisions.put(21700, 11700);tuttime_collisions.put(21800, 21800);tuttime_collisions.put(21900, 21900);
+		tuttime_collisions.put(22000, 22000);
+		tuttime_collisions.put(50800, 50800);tuttime_collisions.put(51000, 51000);tuttime_collisions.put(51200, 51200);tuttime_collisions.put(51400, 51400);
+		tuttime_collisions.put(51600, 51600);
+		
+		lectuttime_collisions.put(10800, 10800);lectuttime_collisions.put(10900, 10900);lectuttime_collisions.put(11000, 11000);lectuttime_collisions.put(11100, 11100);
+		lectuttime_collisions.put(11200, 11200);lectuttime_collisions.put(11300, 11300);lectuttime_collisions.put(11400, 11400);lectuttime_collisions.put(11500, 11500);
+		lectuttime_collisions.put(11600, 11600);lectuttime_collisions.put(11700, 11700);lectuttime_collisions.put(11800, 11800);lectuttime_collisions.put(11900, 11900);
+		lectuttime_collisions.put(12000, 12000);
+		lectuttime_collisions.put(10800, 50800);lectuttime_collisions.put(10900, 50800);lectuttime_collisions.put(11000, 51000);lectuttime_collisions.put(11100, 51000);
+		lectuttime_collisions.put(11200, 51200);lectuttime_collisions.put(11300, 51200);lectuttime_collisions.put(11400, 51400);lectuttime_collisions.put(11500, 51400);
+		lectuttime_collisions.put(11600, 51600);lectuttime_collisions.put(11700, 51600);lectuttime_collisions.put(11800, 51800);lectuttime_collisions.put(11900, 51800);
+		lectuttime_collisions.put(20800, 20800);lectuttime_collisions.put(20800, 20900);lectuttime_collisions.put(20930, 20900);lectuttime_collisions.put(20930, 21000);
+		lectuttime_collisions.put(21100, 21100);lectuttime_collisions.put(21100, 21200);lectuttime_collisions.put(21230, 21200);lectuttime_collisions.put(21230, 21300);
+		lectuttime_collisions.put(21400, 21400);lectuttime_collisions.put(21400, 21500);lectuttime_collisions.put(21530, 21500);lectuttime_collisions.put(21530, 21600);
+		lectuttime_collisions.put(21700, 21700);lectuttime_collisions.put(21700, 21800);lectuttime_collisions.put(21830, 21800);lectuttime_collisions.put(21830, 21900);
+		
 		Slot foo[] = new Slot[times.length];
 		for(int i = 0; i < times.length; i++) {
 			foo[i] = new Slot(times[i]);
@@ -83,7 +117,7 @@ public class Scheduler {
 			for(int j = 0; j < current.length; j++) {
 				tmp_cur[j] = current[j].copy();
 			}
-			Vector<Course> tmp_unas = unassigned.clone();
+			Vector<Course> tmp_unas = (Vector<Course>)unassigned.clone();
 			
 			//pop off unassigned and call the function again
 			tmp_cur[i].addCourse(tmp_unas.remove(0));
@@ -96,9 +130,9 @@ public class Scheduler {
 		Slot solution[] = null;
 		int best = Integer.MIN_VALUE;
 		for(int i = 0; i < possible.size(); i++) {
-			int cur_eval = evaluate(possible[i]);
+			int cur_eval = evaluate(possible.get(i));
 			if(cur_eval > best) {
-				solution = possible[i];
+				solution = possible.get(i);
 			}
 		}
 		return solution;
@@ -106,7 +140,7 @@ public class Scheduler {
 	
 	//evaluate all hard constraint violations to Integer.MIN_VALUE
 	public static int evaluate(Slot[] current) {
-		
+		return 0;
 	}
 }
 
@@ -130,7 +164,7 @@ class Slot {
 	}
 	
 	public Slot copy() {
-		return new Slot(this.course.clone(), time);
+		return new Slot((Vector<Course>)this.course.clone(), time);
 	}
 }
 
@@ -145,5 +179,25 @@ class Course {
 		this.number = number;
 		this.lecture_num = lecture_num;
 		this.is_lecture = is_lecture;
+	}
+}
+
+class Fact {
+	public Slot[] slots = null;
+	public int score = Integer.MIN_VALUE;
+	public Vector<Course> unassigned;
+	
+	public Fact(Vector<Course> unassigned) {
+		this.unassigned = (Vector<Course>)unassigned.clone();
+	}
+	
+	public Fact copy() {
+		Fact tmp = new Fact(unassigned);
+		tmp.score = this.score;
+		tmp.slots = new Slot[this.slots.length];
+		for(int i = 0; i < this.slots.length; i++) {
+			tmp.slots[i] = this.slots[i].copy();
+		}
+		return tmp;
 	}
 }
